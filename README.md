4b30997a6919244598179fc852c94e4e7a5e502d
Saugat Sthapit <mac@172.20.20.20>
2018-02-11 14:20:19 -0500

    Second commit with Readme and Plot changes

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # StockMarket_AI
+
+Used dataframes to collect and mine data from Quandl so that it can be trained to forecast data for future.
+
--- Regression.py
+++ Regression.py
@@ -1,11 +1,13 @@
 import pandas as pd
-import quandl, math
+import quandl, math, datetime
 import numpy as np
-
+import matplotlib.pyplot as plt
+from matplotlib import style
 from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
 from unittest.mock import inplace
 
+style.use('ggplot')
 
 df = quandl.get('WIKI/GOOGL')
 df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
@@ -21,17 +23,41 @@ forecast_out = int(math.ceil(0.01 * len(df)))
 print(forecast_out)
 
 df['label'] = df[forecast_col].shift(-forecast_out)
-df.dropna(inplace=True)
 
 X = np.array(df.drop(['label'] ,1 ))
-y = np.array(df['label'])
 X = preprocessing.scale(X)
+X = X[:-forecast_out] 
+X_lately = X[-forecast_out:] 
+
+
+df.dropna(inplace=True)
+y = np.array(df['label'])
 y = np.array(df['label'])
 
 X_train, X_test, y_train, y_test =  cross_validation.train_test_split(X, y, test_size = 0.2)
 
-clf = LinearRegression()
+clf = LinearRegression(n_jobs =-1)
 clf.fit(X_train, y_train)
 accuracy = clf.score(X_test, y_test)
+forecast_set = clf.predict(X_lately)
+print(forecast_set, accuracy, forecast_out)
+df['Forecast'] = np.nan
 
-print(accuracy)
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+    
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
+
+#print(accuracy)
